067b24fdb2f2b8760e76a630df8882fb4df0c7f6
suaib022 <suaib.109206@gmail.com>
2025-11-18 00:41:07 +0600

    implement functionality of taking inputs and see them in console

--- cv_builder/src/main/java/com/example/cv_builder/FormController.java
+++ cv_builder/src/main/java/com/example/cv_builder/FormController.java
@@ -30,8 +30,17 @@ public class FormController {
     private TextField projects;
 
     @FXML
-    private void handlePreview() {
+    private void handleSubmit() {
         try {
+//            System.out.println(fullName.getText());
+//            System.out.println(phone.getText());
+//            System.out.println(email.getText());
+//            System.out.println(address.getText());
+//            System.out.println(education.getText());
+//            System.out.println(skills.getText());
+//            System.out.println(experience.getText());
+//            System.out.println(projects.getText());
+
             CVData data = new CVData(
                     fullName.getText(),
                     email.getText(),
@@ -43,7 +52,7 @@ public class FormController {
                     projects.getText()
             );
 
-            HelloApplication.showPreview(data);
+//            HelloApplication.showPreview(data);
         } catch (Exception e) {
             e.printStackTrace();
         }
