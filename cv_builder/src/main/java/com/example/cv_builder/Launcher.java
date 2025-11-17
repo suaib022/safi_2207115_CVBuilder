a653cab1e6b640f07dcb19976ea1cdb11b6a5893
suaib022 <suaib.109206@gmail.com>
2025-11-17 23:05:48 +0600

    included homepage with proper navigation to the cv-form page

--- cv_builder/src/main/java/com/example/cv_builder/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.example.cv_builder;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
